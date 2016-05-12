044b316ee2866682542740045a7ed2ae2f71749b
PaoloLabs <paololabsplus@gmail.com>
2016-05-12 01:57:18 -0400

    Adding Register View Controller

---
+++ N10_Application/N10Tests/N10Tests.swift
@@ -0,0 +1,36 @@
+//
+//  N10Tests.swift
+//  N10Tests
+//
+//  Created by Paolo Ramos Mendez on 5/10/16.
+//  Copyright © 2016 Paolo Ramos Mendez. All rights reserved.
+//
+
+import XCTest
+@testable import N10
+
+class N10Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ N10_Application/Pods/Target Support Files/Pods-N10/Pods-N10-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_N10 : NSObject
+@end
+@implementation PodsDummy_Pods_N10
+@end
